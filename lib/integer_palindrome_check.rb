ada20d679eecc9a2fdfc77a95c0713476e55bd7a
Jackie <jonofre16@hotmail.com>
2018-03-28 16:02:02 -0700

    Jackie - integer palindrome - Ampers

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,29 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  if number == nil || number < 0
+    return false
+  elsif number < 100
+    return true
+  else
+  end
+
+  result = 0
+  num = number
+
+  while num != 0
+
+    r = num%10
+
+    result = result*10 + r
+
+    num = num / 10
+
+  end
+
+  if result == number
+    return true
+  else
+    return false
+  end
+
 end
